f78cb80d5a6def7a6a4d405d79b6533a033f3a2a
juanAllende <allendeseptimo@gmail.com>
2024-06-03 00:42:04 -0400

    final

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-precio = 400000
+document.addEventListener("DOMContentLoaded", () => {
+  const precio = 400000;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+  const precioSpan = document.querySelector(".precio-inicial");
+  const increaseButton = document.querySelector("#increase");
+  const decreaseButton = document.querySelector("#decrease");
+  const quantityDisplay = document.querySelector(".cantidad");
+  const totalDisplay = document.querySelector(".valor-total");
+
+  let quantity = 0;
+
+  precioSpan.innerHTML = precio;
+
+  function updateTotal() {
+    const total = quantity * precio;
+    totalDisplay.innerHTML = total.toFixed(0); // No mostrar decimales
+  }
+
+  increaseButton.addEventListener("click", () => {
+    quantity++;
+    quantityDisplay.innerHTML = quantity;
+    updateTotal();
+  });
+
+  decreaseButton.addEventListener("click", () => {
+    if (quantity > 0) {
+      quantity--;
+      quantityDisplay.innerHTML = quantity;
+      updateTotal();
+    }
+  });
+});
